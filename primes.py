94cafcb7392723d6356b3f238bd6a44ff03d8758
Amber <amberwyleung@gmail.com>
2022-12-04 14:02:47 +0000

    complete primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,16 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0:
+        raise ValueError
+    list.append(2)
+    counter = 3
+    while len(list) is not number_of_primes:
+        common_factor = 0
+        for i in range(2, counter+1):
+            if counter % i == 0:
+                common_factor += 1
+        if common_factor == 1:
+            list.append(counter)
+        counter += 1
     return list
